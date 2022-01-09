760803f3dc535882513becc9848d6f9119ae5a54
Lea van den Heuvel <80387555+leavdheuvel@users.noreply.github.com>
2022-01-09 22:23:16 +0100

    Begin gemaakt met dashboard
    En wat toevoegingen aan model

---
+++ Dashboard_tijdreeksen_YvdW_LvdH_2122.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 20:35:32 2022
+
+@author: Lea
+
+Dashboard tijdreeksen 2122
+Ymke van der Waal en Lea van den Heuvel (18057020)
+
+Dit is de code voor de dashboard behorende bij de eindopdracht.
+
+Om de app te runnen:
+1. Run dit bestand in prompt (python filename.py)
+2. Ga naar http://127.0.0.1:8050/
+
+"""
+
